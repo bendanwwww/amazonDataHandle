f508fd50886fdb5db9df33981d652e48e4fc7943
bendanwwww <461806307@qq.com>
2022-08-11 19:38:00 +0800

    track order update

--- com/data/handle/track_order_handle.py
+++ com/data/handle/track_order_handle.py
@@ -11,12 +11,12 @@ def read_file(filepath):
 
 code_path = os.path.abspath(os.path.dirname(__file__))
 
-ret = read_file(code_path + '/20220810_order.csv').split('\n')
+ret = read_file(code_path + '/test.csv').split('\n')
 url = 'https://t.17track.net/restapi/track'
 
 track_order = []
 index = 0
-last = 792
+last = 0
 for r in ret:
     if index < last:
         index += 1
@@ -44,15 +44,53 @@ track_status_map = {
     }
 
 track_org_map = {
-        "DPD": 100007,
-        "DHL": 7041
-    }
+    "DPD": 100007,
+    "DHL": 7041
+}
+
+# 物流商目前全为DPD
+org = 'DPD'
+
+# 先执行一次 减少报错
+index = 0
+for order_data in track_order:
+    order = order_data
+    curl = """
+curl 'https://t.17track.net/restapi/track' \
+-H 'authority: t.17track.net' \
+-H 'sec-ch-ua: "Chromium";v="92", " Not A;Brand";v="99", "Google Chrome";v="92"' \
+-H 'accept: application/json, text/javascript, */*; q=0.01' \
+-H 'x-requested-with: XMLHttpRequest' \
+-H 'sec-ch-ua-mobile: ?0' \
+-H 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36' \
+-H 'content-type: application/x-www-form-urlencoded; charset=UTF-8' \
+-H 'origin: https://t.17track.net' \
+-H 'sec-fetch-site: same-origin' \
+-H 'sec-fetch-mode: cors' \
+-H 'sec-fetch-dest: empty' \
+-H 'referer: https://t.17track.net/zh-cn' \
+-H 'accept-language: zh-CN,zh;q=0.9' \
+-H 'cookie: country=CN; _yq_bid=G-B8E4326D94D89642; v5_Culture=zh-cn; _ga=GA1.2.285024683.1660130328; _gid=GA1.2.1634478300.1660130328; __gads=ID=e4e6a71a3b12e5e4:T=1660130333:S=ALNI_MYX91hQgUJ8ZceV1mybZNfFasXc7w; __gpi=UID=00000876d1c51ace:T=1660130333:RT=1660130333:S=ALNI_MbrMjo2UWAdh4gNW38M-pw5KgqbkQ; crisp-client/session/115772b1-4fc7-471c-a364-05246aac2f53=session_24d6836e-0a85-4594-ae36-986780cb731f; _ati=163270314626; uid=80915BFE84E94E7BBD72D306C4206F4C; _yq_rc_=yq.5.301.zh-cn.0.0.4095936322528469225; v5_TranslateLang=zh-Hans; v5_HisExpress=100007; Last-Event-ID=657572742f6435332f64663935336437383238312f343966393038383031633a303731373638343331323a65736c61663a6c742d696e612d7179207466656c2d756e656d2d6e776f64706f726420756e656d2d6e776f64706f72642223132eda1909c9f86e' \
+--data-raw '{\"data\": [{\"num\": \"""" + order + """\",\"fc\": """+ str(track_org_map[org]) +""",\"sc\": 0}],\"guid\": \"\",\"timeZoneOffset\": -480}' \
+--compressed
+"""
+    res = json.loads(os.popen(curl).readlines()[0])
+    # 获取guid
+    try:
+        guid = res['g']
+    except Exception as e:
+        print(e)
+        continue
+    print("预处理: " + str(index))
+    index += 1
+    time.sleep(0.5)
+
 
 index = 0
 res_data = []
+error_order = []
 for order_data in track_order:
     order = order_data
-    org = 'DPD'
     curl = """
 curl 'https://t.17track.net/restapi/track' \
 -H 'authority: t.17track.net' \
@@ -133,14 +171,19 @@ curl 'https://t.17track.net/restapi/track' \
         #     track_time_str = '超过' + str(int((int(time.time()) - timeStamp) / (24 * 60 * 60))) + '天 最后更新时间 ' + track_time
         # res_data.append([str(order).replace(' ', ''), str(track_status).replace(' ', ''), track_time_str, org.replace(' ', '')])
     except Exception as e:
-        print(res_data)
+        error_order.append(order)
         print(e)
         # res_data.append([str(order).replace(' ', ''), '', '', org.replace(' ', '')])
     print(index)
     index += 1
 
-filename = code_path + '/track_order_20220810.txt'
+filename = code_path + '/test_20220811.txt'
 with open(filename, 'w') as file_object:
     for d in res_data:
         file_object.write(d[0] + '\t' + d[1] + '\t' + d[2] + '\n')
+
+filename = code_path + '/test_20220811_error.txt'
+with open(filename, 'w') as file_object:
+    for d in error_order:
+        file_object.write(d + '\n')
 
